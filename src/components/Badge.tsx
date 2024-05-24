75e115e9b2eb56a75dc7f8f95217e8585df16cf2
jsu-github <johnsuijker@gmail.com>
2024-05-25 00:48:40 +0200

    nog een aanpassing

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -48,7 +48,7 @@ export const Badge = (props: Props) => {
             'background-color': props.badgeBackgroundColor ?? '#ffffff',
           }}
         >
-          {props.footer?.text ?? 'Powered by'}
+          {props.footer?.text ?? '...'}
           <a
             ref={liteBadge}
             href={props.footer?.companyLink ?? ''}
@@ -58,7 +58,7 @@ export const Badge = (props: Props) => {
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Flowise'}</span>
+            <span>&nbsp;{props.footer?.company ?? '...'}</span>
           </a>
         </span>
       </Show>
