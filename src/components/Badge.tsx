3856f89370c3c1da64d9de92c1c61deb5f1cb694
jsu-github <78882350+jsu-github@users.noreply.github.com>
2024-05-24 21:40:20 +0200

    Orthowiz21

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,7 +54,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Orthowize</span>
+        <span> Orthowize1</span>
       </a>
     </span>
   );
